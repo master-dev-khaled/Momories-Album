b9f10af2965207e93b9e11d257816a81434e5629
master-dev-khaled <khaledprogg20@gmail.com>
2024-02-26 16:39:27 +0100

    Client Side Features: FEAT[1]: Create the main components (form,cards,header). FEAT[2]: Getting and sending APIs to the server side with axios. FEAT[3]: Managing methods and states with redux: 	- implementing the actions. 	- implementing the reducers.
    Server Side Features:
    FEAT[1]: Creating the schema.
    FEAT[2]: Creating the controllers that manage the following methods:
    	- fetch all posts.
    	- add post to the database.
    	- update post informations.
    	- like post.
    	- delete post.
    FEAT[3]: Manage the env variables and add some modifications to set the
    app for deploiment.

---
+++ server/index.js
@@ -0,0 +1,24 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import postRoutes from './routes/posts.js';
+dotenv.config()
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use((req, res, next) => {
+  res.send("welcome to memories!")
+})
+const CONNECTION_URL = 'mongodb://0.0.0.0:27017/hotelOrganizer';
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
